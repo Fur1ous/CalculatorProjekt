811d5e0d3a7aad648dec60788955de139194a493
Rapp <Rapp@N2001EZAA>
2016-04-19 13:54:02 +0200

    Projektdateien hinzufügen.

---
+++ CalculatorProjekt/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CalculatorProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte Zahl 1 eingeben.");
+            string zahl1 = Console.ReadLine();
+
+            Console.WriteLine("Bitte Zahl 2 eingeben.");
+            string zahl2 = Console.ReadLine();
+        }
+    }
+}
