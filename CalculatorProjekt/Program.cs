7af17239dd130a8a4f9d2316aa9a944b5139aaaa
Rapp <Rapp@N2001EZAA>
2016-04-19 14:30:25 +0200

    bla

--- CalculatorProjekt/Program.cs
+++ CalculatorProjekt/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CalculatorLib;
+using System;
 
 namespace CalculatorProjekt
 {
@@ -6,11 +7,35 @@ namespace CalculatorProjekt
     {
         static void Main(string[] args)
         {
+            Punkt p1;
+            Punkt p2;
+
             Console.WriteLine("Bitte Zahl 1 eingeben.");
             string zahl1 = Console.ReadLine();
 
             Console.WriteLine("Bitte Zahl 2 eingeben.");
             string zahl2 = Console.ReadLine();
+
+            float z1 = 0;
+            float z2 = 0;
+
+            try
+            {
+                z1 = float.Parse(zahl1);
+                z2 = float.Parse(zahl2);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Ergebnis: " + (z1 + z2));
+
         }
     }
 }
